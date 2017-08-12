3cef1a2b63918011af39d72fa9962fccc730ba65
Mohsan <moabbas.ch@gmail.com>
2017-08-12 19:11:45 -0300

    changed

--- colorgame.js
+++ colorgame.js
@@ -1,12 +1,12 @@
 var colors = [
-    "rgb(126, 49, 4)",
+    "rgb(242, 133, 7)",
     "rgb(23, 191, 193)",
     "rgb(11, 185, 7)",
-    "rgb(245, 163, 4)",
+    "rgb(219, 17, 250)",
     "rgb(23, 23, 3)",
-    "rgb(255,10,50)"
-
+    "rgb(255, 10, 50)"
 ];
+
 var message = document.querySelector("#message");
 var colorPicked = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
